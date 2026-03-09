036e907f31dab032499fd81b788d5e77b3013fa1
Ritvik Vats <poo09@Ritviks-MacBook-Air.local>
2026-03-09 23:00:41 +0530

    Cleanup: remove junk files (.DS_Store, test files, package.json) from git tracking

--- test_pdf.py
+++
@@ -1,8 +0,0 @@
-from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Paragraph
-from reportlab.lib.styles import getSampleStyleSheet
-import markdown2
-import re
-
-html = markdown2.markdown("**Hello** World!")
-print("HTML:", html)
